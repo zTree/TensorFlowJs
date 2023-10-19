5a4f127413e3a1e6e0009e2940d2ce5d9046ad00
zhangqigang <zhangqigang@ones.cn>
2023-10-19 11:45:33 +0800

    style

--- src/component/content/digit_recognition.tsx
+++ src/component/content/digit_recognition.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 // import { useTranslation } from 'react-i18next';
 import { styled } from '@mui/material/styles';
+// import { CustomTheme, MediaStyles } from "../../common/config";
 
 interface DigitRecognitionProps {
 }
@@ -10,12 +11,20 @@ const DigitRecognitionContent = styled('div')({
   // display: 'flex'
 });
 
+const Iframe = styled('iframe')(() => ({
+  border: 0,
+  verticalAlign: 'bottom',
+  width: '100%',
+  height: '100%',
+}));
+
+
 const DigitRecognition = memo((props: DigitRecognitionProps) => {
   // const { t } = useTranslation();
 
   return (
     <DigitRecognitionContent {...props} >
-      <iframe id='digit-recognition' />
+      <Iframe id='digit-recognition' />
     </DigitRecognitionContent>
   );
 });
