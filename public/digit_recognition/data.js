6803280899011719aac202f127b56b624a93d6e0
zhangqigang <zhangqigang@ones.cn>
2023-10-28 20:54:31 +0800

    update assets files path

--- public/digit_recognition/data.js
+++ public/digit_recognition/data.js
@@ -23,9 +23,11 @@ const NUM_TRAIN_ELEMENTS = 55000;
 const NUM_TEST_ELEMENTS = NUM_DATASET_ELEMENTS - NUM_TRAIN_ELEMENTS;
 
 const MNIST_IMAGES_SPRITE_PATH =
-    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
+    'https://treejs.cn/assets/tensorflow/digit_recognition/mnist_images.png';
+    // 'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
 const MNIST_LABELS_PATH =
-    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
+    'https://treejs.cn/assets/tensorflow/digit_recognition/mnist_labels_uint8';
+    // 'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
 
 /**
  * A class that fetches the sprited MNIST dataset and returns shuffled batches.
